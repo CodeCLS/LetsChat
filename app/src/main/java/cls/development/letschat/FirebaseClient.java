1f56b6b81dacd92f6d3dc2fb6df9b8028896f32d
Caleb <primocaleb@gmail.com>
2020-12-24 16:26:43 +0100

    firebase send verification code applied method

--- app/src/main/java/cls/development/letschat/FirebaseClient.java
+++ app/src/main/java/cls/development/letschat/FirebaseClient.java
@@ -1,12 +1,23 @@
 package cls.development.letschat;
 
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class FirebaseClient {
     public static FirebaseClient dataRepository;
     private FirebaseUser firebaseUser;
     private FirebaseAuth mAuth;
+    private PhoneAuthOptions options;
     public synchronized static FirebaseClient getInstance(){
         if(dataRepository == null)
             dataRepository= new FirebaseClient();
@@ -28,8 +39,15 @@ public class FirebaseClient {
     public boolean currentlyLoggedIn(){
         return firebaseUser != null;
     }
-    public boolean signUpUser(){
-        work
+    public void sendVerificationCode(String number, Activity activity, PhoneAuthProvider.OnVerificationStateChangedCallbacks callback){
+        options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(number)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(activity)                 // Activity (for callback binding)
+                        .setCallbacks(callback)  // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 
 }
